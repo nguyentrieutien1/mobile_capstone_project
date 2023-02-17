9bad543f3f55cf5fc394952e1bcc563955d69330
Trieu Tien Nguyen <59260191+nguyentrieutien1@users.noreply.github.com>
2023-02-17 15:43:15 +0700

    config route

--- App.js
+++ App.js
@@ -1,38 +1,68 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {Button, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Button} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 function HomeScreen({navigation}) {
   return (
-    <Button
-      title="Click"
-      onPress={() => {
-        navigation.navigate('detail');
-      }}></Button>
-  );
-}
-function DetailScreen() {
-  return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Detail Screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
     </View>
   );
 }
-const Stack = createNativeStackNavigator();
 
-function App() {
+function ProfileScreen({navigation}) {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="detail" component={DetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        title="Go to Notifications"
+        onPress={() => navigation.navigate('Notifications')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
   );
 }
 
-export default App;
+function NotificationsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SettingsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ src/routes/MyStack.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MyStack() {
+  return <Text>Nguyen Thanh Tung</Text>;
+}
